04571a02cc57c070ccc246ff80814d243604decd
Sheilf <37036296+Sheilf@users.noreply.github.com>
2018-05-15 18:47:00 -0500

    Setting basic stage

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import logo from './logo.png';
+import './Header.css';
+
 
 const Header = () => {
 
     return(
-        <div>Header</div>
+        <div className="Header">
+            <img src={logo} alt="react-coin-logo" className="Header-logo" />
+        </div>
     )
 }
+
+export default Header;
