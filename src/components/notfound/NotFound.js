87f4aceeca7d62b99ebf4091d9fcc2cae325945f
Sheilf <37036296+Sheilf@users.noreply.github.com>
2018-05-19 22:08:23 -0500

    Create NotFound.js
    Page that displays when routing to invalid page

---
+++ src/components/notfound/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+const NotFound = () =>{
+
+    return(
+        <div className="NotFound">
+            <h1 className="NotFound-title"> The page you're looking for is not valid </h1>
+            <Link to="/" className="NotFound-link">Go back to Main Page </Link>
+        </div>
+    )
+}
+
+export default NotFound;
