12385463e72914832fe72f195216877ee9752192
Sheilf <37036296+Sheilf@users.noreply.github.com>
2018-05-20 13:32:04 -0500

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const handleResponse = (response) => {
     /*
         Fetch error helper.
@@ -8,4 +10,17 @@ export const handleResponse = (response) => {
             return response.ok ? json : Promise.reject(json);
         }
     );
+}
+
+export const renderChangePercent = (percent) => {
+    if (percent > 0) {
+        // &uarr; -> up arrow text symbol
+      return <span className="percent-raised">{percent}% &uarr;</span>
+    } else if (percent < 0) {
+        //&darr; -> down arrow text symbol
+      return <span className="percent-fallen">{percent}% &darr;</span>
+    } else {
+        //..
+      return <span>{percent}</span>
+    }
 }
