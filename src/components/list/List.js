d10a43ea8d99b778fed8e23dbf9ef813aeba10ed
Sheilf <37036296+Sheilf@users.noreply.github.com>
2018-05-20 13:31:31 -0500

    Update List.js

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
 import Loading from '../common/Loading';
@@ -68,18 +67,7 @@ class List extends React.Component{
             }
         );
     }
-    renderChangePercent(percent) {
-        if (percent > 0) {
-            // &uarr; -> up arrow text symbol
-          return <span className="percent-raised">{percent}% &uarr;</span>
-        } else if (percent < 0) {
-            //&darr; -> down arrow text symbol
-          return <span className="percent-fallen">{percent}% &darr;</span>
-        } else {
-            //..
-          return <span>{percent}</span>
-        }
-    }
+
 
     handlePaginationClick(direction){
         let nextPage = this.state.page;
@@ -114,10 +102,10 @@ class List extends React.Component{
                     totalPages={totalPages}
                     handlePaginationClick={this.handlePaginationClick}
                 />
-                
+
                 <Table
                     currencies={currencies}
-                    renderChangePercent={this.renderChangePercent}
+              
                 />
 
 
