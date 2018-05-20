357651cb7d9d187bfe0af4836207ba881ec06773
Sheilf <37036296+Sheilf@users.noreply.github.com>
2018-05-20 13:28:38 -0500

    Add return home Link

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './logo.png';
 import './Header.css';
 
@@ -7,7 +8,11 @@ const Header = () => {
 
     return(
         <div className="Header">
-            <img src={logo} alt="react-coin-logo" className="Header-logo" />
+        <Link to="/">
+            <img src={logo}
+                 alt="react-coin-logo" 
+                 className="Header-logo" />
+        </Link>      
         </div>
     )
 }
