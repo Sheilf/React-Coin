b104bea1bce5564d37c90a332c3b6a48f93cb0ab
Sheilf <37036296+Sheilf@users.noreply.github.com>
2018-05-22 21:40:39 -0500

    Final update

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from './logo.png';
 import './Header.css';
+import Search from './Search';
 
 
 const Header = () => {
 
     return(
         <div className="Header">
-        <Link to="/">
-            <img src={logo}
-                 alt="react-coin-logo" 
-                 className="Header-logo" />
-        </Link>      
+            <Link to="/">
+                <img src={logo}
+                    alt="react-coin-logo" 
+                    className="Header-logo" />
+            </Link>      
+            <Search />
         </div>
     )
 }
