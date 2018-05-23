3199ba6bed52f3f175bab67261e5938b8c7ebebf
Sheilf <37036296+Sheilf@users.noreply.github.com>
2018-05-22 21:32:18 -0500

    final update

--- src/components/notfound/NotFound.js
+++ src/components/notfound/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () =>{
     return(
         <div className="NotFound">
             <h1 className="NotFound-title"> The page you're looking for is not valid </h1>
-            <Link to="/" className="NotFound-link">Go back to Main Page </Link>
+            <Link to="/" className="NotFound-link">Go back to Main Page</Link>
         </div>
     )
 }
